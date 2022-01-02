64dda0b77b38eacca136438b9de208aaa4dac1b5
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-03 02:02:36 +0530

    Create GoldmanSachs_Question3.java

---
+++ GoldmanSachs_Question3.java
@@ -0,0 +1,32 @@
+Submission Link: https://practice.geeksforgeeks.org/viewSol.php?subId=27acdc285b7b1b8e5e7182164b47d92d&pid=703804&user=gopalsays108
+
+class Solution {
+    
+    public int countSubArrayProductLessThanK(long a[], long n, long k)
+    {
+        if(k < 1) return 0;
+        
+        //sliding window approach
+        int result = 0;
+        long prod = 1;
+        
+        int left = 0;
+        int right =0;
+        
+        while(right < a.length){
+            
+            prod *= a[right];
+            
+            while(prod >= k){
+                prod /= a[left];
+                left++;
+            }
+            
+            result += right - left +1;
+            right++;
+        }
+        
+        return result;
+        
+    }
+}
