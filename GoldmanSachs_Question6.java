5f4bf15d73a74d7f42eb7429e07a57a6761ed1e7
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-03 23:20:46 +0530

    Create GoldmanSachs_Question6.java

---
+++ GoldmanSachs_Question6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        
+        String lhs = str1 + str2;
+        String rhs = str2 + str1;
+        
+        if(str1.equals(str2)){
+            return str1;
+        }
+            
+        if(!lhs.equals(rhs)){
+            return  "";
+        }
+        
+        if(str1.length() > str2.length()){
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+        }else{
+           return gcdOfStrings(str2.substring(str1.length()), str1);
+        }       
+        
+    }
+}                   
