0d3fd639c498db221b825779a8e575a53bcc53cc
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-10 13:54:19 +0530

    Create 8Question.java

---
+++ 8Question.java
@@ -0,0 +1,12 @@
+//https://practice.geeksforgeeks.org/viewSol.php?subId=c2b32f109999e59d100535f5988eb2fc&pid=701419&user=gopalsays108
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+        // your code here
+        
+        return (long) (m/2)+1;
+    }    
+}
