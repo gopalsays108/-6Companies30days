96aa0917fc1697e323f115d5824b3dc0373faa07
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-15 18:24:20 +0530

    Create 15Question.java

---
+++ microsoft Questions/15Question.java
@@ -0,0 +1,35 @@
+//lINK: https://practice.geeksforgeeks.org/viewSol.php?subId=8e700a7546ce65db2a437c0312313f8d&pid=700494&user=gopalsays108
+class Solution{
+    public String findOrder(String [] dict, int N, int K)    {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i = 0 ; i < K ; i++)graph.add(new ArrayList<>());
+        for(int i = 1 ; i < N ; i++){
+            String a = dict[i-1];
+            String b = dict[i];
+            int n = a.length();
+            int m = b.length();
+            for(int j = 0 ; j < Math.min(n,m) ; j++){
+                if(a.charAt(j) != b.charAt(j)){
+                    ArrayList<Integer> al = graph.get(a.charAt(j) - 'a');
+                    al.add(b.charAt(j) - 'a');
+                    break;
+                }
+            }
+        }
+        boolean visited [] = new boolean[K];
+        for(int i = 0 ; i < K ; i++){
+            if(!visited[i])solve(sb, visited, i, graph);
+        }
+        return sb.reverse().toString();
+    }
+    public static void solve(StringBuilder sb, boolean visited[] , int node, ArrayList<ArrayList<Integer>> graph){
+        if(visited[node])return;
+        visited[node] = true;
+        ArrayList<Integer> neighbour = graph.get(node);
+        for(int neigh : neighbour){
+            solve(sb, visited, neigh, graph);
+        }
+        sb.append((char)('a' + node));
+    }
+}
