e979002375f59faeef292ff28eed199c80a7d700
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-15 18:03:30 +0530

    Create 1Question.java

---
+++ microsoft Questions/1Question.java
@@ -0,0 +1,29 @@
+//Link: https://practice.geeksforgeeks.org/viewSol.php?subId=85980bc7f2992254f50d7f4677de9e36&pid=704140&user=gopalsays108
+
+class Solution
+{
+
+	public int minDifference(int arr[], int n) 
+	{ 
+	    int sum = 0;
+	    for(int i : arr)sum += i;
+	    int temp = sum/2;
+	    boolean dp [][] = new boolean[n+1][temp+1];
+	    for(int i = 0 ; i <= n ; i++)dp[i][0] = true;
+	    for(int i = 1 ; i <= n ; i++){
+	        for(int j = 1 ; j <= temp ; j++){
+	            if(arr[i-1] <= j && dp[i-1][j-arr[i-1]]){
+                   dp[i][j] = true;	                
+	            }else{
+	                dp[i][j] = dp[i-1][j];
+	            }
+	        }
+	    }
+	    for(int i = temp ; i >= 0 ; i--){
+	        if(dp[n][i]){
+	            return (sum - (2*i));
+	        }
+	    }
+	    return 0;
+	} 
+}
