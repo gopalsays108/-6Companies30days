29a6d280a6a23b5ab187ef5cc2dc1cfb66e077dc
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-15 18:05:15 +0530

    Create 3Question.java

---
+++ microsoft Questions/3Question.java
@@ -0,0 +1,38 @@
+//Link: https://practice.geeksforgeeks.org/viewSol.php?subId=c3a41c3811addb8e31736ab241b3cf3c&pid=701989&user=gopalsays108
+
+class GFG
+{
+    static void rotate(int matrix[][]) 
+    {
+        int n = matrix.length;
+        for(int i = 0 ; i < n ; i++){
+            int idx = i;
+            int jdx = 0;
+            for(int te = 0 ; te < (i+1)/2 ; te++){
+                int temp = matrix[idx][jdx];
+                matrix[idx][jdx] = matrix[jdx][idx];
+                matrix[jdx][idx] = temp;
+                idx--;jdx++;
+            }
+        }
+        for(int i = 1 ; i < n ; i++){
+            int idx = n-1;
+            int jdx = i;
+            for(int te = 0 ; te < (n-i)/2 ; te++){
+                int temp = matrix[idx][jdx];
+                matrix[idx][jdx] = matrix[jdx][idx];
+                matrix[jdx][idx] = temp;
+                idx--;jdx++;
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            int idx = 0, jdx = n-1;
+            while(idx < jdx){
+                int temp = matrix[idx][i];
+                matrix[idx][i] = matrix[jdx][i];
+                matrix[jdx][i] = temp;
+                idx++;jdx--;
+            }
+        }
+    }
+}
