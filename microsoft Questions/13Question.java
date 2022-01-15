246eec4a426458b1fc39e5562afeba26bc606df9
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-15 18:22:28 +0530

    Create 13Question.java

---
+++ microsoft Questions/13Question.java
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/viewSol.php?subId=726254e4579c33dfc43235eb100b6a6a&pid=700463&user=gopalsays108
+class Solution
+{
+    //Function to find if the given edge is a bridge in graph.
+    static int isBridge(int V, ArrayList<ArrayList<Integer>> adj,int c,int d)
+    {
+        boolean [] isVisited = new boolean[V];
+        solve(c , adj, c, d, isVisited);
+        if(isVisited[c] && isVisited[d])return 0;
+        return 1;
+    }
+    public static void solve(int node, ArrayList<ArrayList<Integer>> graph, int c , int d , boolean [] isVisited){
+        if(isVisited[node])return;
+        isVisited[node] = true;
+        ArrayList<Integer> neighbour = graph.get(node);
+        for(int neigh : neighbour){
+            if((node == c && neigh == d )|| (node == d && neigh == c))continue;
+            solve(neigh, graph, c, d, isVisited);
+        }
+    }
+}
