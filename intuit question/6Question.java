c1e0fd54a0396bd695877ee8570b33e80a68354d
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-25 21:22:28 +0530

    Create 6Question.java

---
+++ intuit question/6Question.java
@@ -0,0 +1,23 @@
+//https://practice.geeksforgeeks.org/viewSol.php?subId=13363cfd2162e29c963ab29b2d873af8&pid=702078&user=gopalsays108
+class Solution {
+    
+    public List<String> AllParenthesis(int n) 
+    {
+        // Write your code here
+        List<String> arr = new ArrayList<>();
+        backtrack(arr,"",0, 0, n);
+        return arr;
+        
+    }
+    
+    
+    public void backtrack(List<String> arr, String current_string, int open, int close, int max){
+        if(current_string.length() == max * 2){ //Some base case
+            arr.add(current_string);
+            return;
+        }
+        
+        if(open < max) backtrack(arr, current_string + "(", open + 1 , close, max );
+        if(close < open) backtrack(arr, current_string + ")", open, close + 1, max);
+    }
+}
