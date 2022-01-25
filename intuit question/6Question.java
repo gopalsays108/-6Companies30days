1fa6b1a615c4b99661df5cb4271a3d4381a37b47
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-25 21:23:41 +0530

    Update 6Question.java

--- intuit question/6Question.java
+++ intuit question/6Question.java
@@ -1,23 +1,56 @@
-//https://practice.geeksforgeeks.org/viewSol.php?subId=13363cfd2162e29c963ab29b2d873af8&pid=702078&user=gopalsays108
+/**https://leetcode.com/submissions/detail/627604875/
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
 class Solution {
-    
-    public List<String> AllParenthesis(int n) 
-    {
-        // Write your code here
-        List<String> arr = new ArrayList<>();
-        backtrack(arr,"",0, 0, n);
-        return arr;
-        
-    }
-    
-    
-    public void backtrack(List<String> arr, String current_string, int open, int close, int max){
-        if(current_string.length() == max * 2){ //Some base case
-            arr.add(current_string);
-            return;
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n = mountainArr.length();
+        int l = 0, r = n-1;
+        int peek = -1;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            int left = mountainArr.get(mid-1);
+            int right = mountainArr.get(mid+1);
+            int center = mountainArr.get(mid);
+            if(left < center && center > right){
+                peek = mid;
+                break;
+            }else if(left < center && center < right){
+                l = mid;
+            }else r = mid;
         }
-        
-        if(open < max) backtrack(arr, current_string + "(", open + 1 , close, max );
-        if(close < open) backtrack(arr, current_string + ")", open, close + 1, max);
+        int ans = inc(mountainArr, target, 0 , peek);
+        if(ans != -1)return ans;
+        else ans = dec(mountainArr, target, peek+1, n-1);
+        return ans;
+    }
+    public static int inc(MountainArray arr, int target, int l , int r){
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            int val = arr.get(mid);
+            if(val == target){
+                return mid;
+            }else if(val < target){
+                l = mid+1;
+            }else r = mid-1;
+        }
+        return -1;
+    }
+    public static int dec(MountainArray arr, int target, int l , int r){
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            int val = arr.get(mid);
+            if(val == target){
+                return mid;
+            }else if(val > target){
+                l = mid+1;
+            }else r = mid-1;
+        }
+        return -1;
     }
 }
