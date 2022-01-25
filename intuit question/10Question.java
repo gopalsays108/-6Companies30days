1b3a91288bfa6d8964b804fdc1a1c929c40e27b2
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-25 21:27:07 +0530

    Create 10Question.java

---
+++ intuit question/10Question.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/submissions/detail/627607017/
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int v = isConnected.length;
+        int ans = 0;
+        boolean visited[] = new boolean[v];
+        for(int i = 0 ; i < v ; i++){
+            if(!visited[i]){
+                ans++;
+                solve(isConnected, visited, i);
+            }
+        }
+        return ans;
+    }
+    public static void solve(int [][] graph, boolean visited [] , int node){
+        if(visited[node])return;
+        visited[node] = true;
+        for(int i = 0 ; i < graph.length ; i++){
+            if(graph[node][i] == 1){
+                solve(graph, visited, i);
+            }
+        }
+    }
+}
