8372223a9ee285f2f34eb3c812979be7e45c9887
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-01-30 21:01:00 +0530

    Create 4Question.java

---
+++ walmart/4Question.java
@@ -0,0 +1,17 @@
+//https://practice.geeksforgeeks.org/viewSol.php?subId=0a3a7447d8499a7fd8addce8b69e5f76&pid=701714&user=gopalsays108
+class Solution
+{
+    //Function to find total number of unique paths.
+    public static int NumberOfPath(int a, int b) 
+    {
+        int [][] arr = new int[a][b];
+        return solve(arr, a, b, 0 , 0 , 0);
+    }
+    public static int solve(int arr[][], int n, int m, int x, int y, int ans){
+        if(x >= n || y >= m)return ans;
+        if(x == n-1 && y == m-1)return ans+1;
+        ans = solve(arr,n,m,x+1,y,ans);
+        ans = solve(arr,n,m,x,y+1,ans);
+        return ans;
+    }
+}
